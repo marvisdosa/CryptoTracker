f470e5b37d7051e944dfae5cfedad24779952ed6
Marvis Ighedosa <dosamarvis@gmail.com>
2023-12-15 22:29:03 +0100

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Marvis Ighedosa on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
