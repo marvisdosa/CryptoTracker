4ca218a0960528335bec8707c1f462acd7104c03
Marvis Ighedosa <dosamarvis@gmail.com>
2023-12-18 11:47:12 +0100

    Changed App background Colour

--- CryptoTracker/ContentView.swift
+++ CryptoTracker/ContentView.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ContentView.swift
 //  CryptoTracker
 //
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        HomeView()
     }
 }
 
---
+++ CryptoTracker/View/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeView.swift
+//  CryptoTracker
+//
+//  Created by Marvis Ighedosa on 18/12/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color(.green).ignoresSafeArea()
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
